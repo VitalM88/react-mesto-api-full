fd199e47dd2685f0d14aac53efc46edba38f8cff
Vitalii Matveenkov <vitalii_matveenkov@mail.ru>
2022-10-13 14:25:47 +0300

    edit jwt

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -93,7 +93,7 @@ module.exports.login = (req, res, next) => {
     .then((user) => {
       const token = jwt.sign(
         { _id: user._id },
-        NODE_ENV ? JWT_SECRET : 'dev-secret',
+        NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
         { expiresIn: '7d' },
       );
       res.send({ token });
---
+++ backend/test_jwt.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken');
+
+const YOUR_JWT = 'sInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzQ1N2EyYmY5YjZlNTJiNGM1OWEwZGYiLCJpYXQiOjE2NjU2NTk5ODcsImV4cCI6MTY2NjI2NDc4N30.T9AWfwvoHyA7xtuLbn0pkWeMv1nrq4i0EjaJu4JO7-I'; // вставьте сюда JWT, который вернул публичный сервер
+const SECRET_KEY_DEV = 'dev-secret'; // вставьте сюда секретный ключ для разработки из кода
+
+try {
+  const payload = jwt.verify(YOUR_JWT, SECRET_KEY_DEV);
+  console.log('\x1b[31m%s\x1b[0m', `
+    Надо исправить. В продакшне используется тот же
+    секретный ключ, что и в режиме разработки.
+  `);
+} catch (err) {
+  if (err.name === 'JsonWebTokenError' && err.message === 'invalid signature') {
+    console.log(
+      '\x1b[32m%s\x1b[0m',
+      'Всё в порядке. Секретные ключи отличаются',
+    );
+  } else {
+    console.log(
+      '\x1b[33m%s\x1b[0m',
+      'Что-то не так',
+      err,
+    );
+  }
+}
